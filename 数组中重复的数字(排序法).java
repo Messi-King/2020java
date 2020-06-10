832e37bef90017ec4810fd83984534e58dc4bda6
Java <15591459159@163.com>
2020-06-10 22:13:25 +0800

    (排序法)

---
+++ 数组中重复的数字(排序法).java
@@ -0,0 +1,11 @@
+//排序法
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i + 1])
+                return nums[i];
+        }
+        return -1;
+    }
+}
