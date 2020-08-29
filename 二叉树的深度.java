42392a3615b75d8b79adcb6f01976dde76098cc3
Java <15591459159@163.com>
2020-08-29 10:35:56 +0800

    二叉树的深度

---
+++ 二叉树的深度.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int maxDepth(TreeNode root) {
+      if(root == null) return 0;
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        if(leftHeight > rightHeight) {
+            return leftHeight+1;
+        }
+        return rightHeight+1;
+    }
+}
