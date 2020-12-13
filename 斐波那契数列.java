b70bafbb6ada5185cf54c71d973afd031586e17e
Java <15591459159@163.com>
2020-12-13 14:21:05 +0800

    Fibonacci

--- 斐波那契数列.java
+++ 斐波那契数列.java
@@ -2,13 +2,13 @@ public class Solution {
     public int Fibonacci(int n) {
         //递归
          if(n == 1 || n == 2) {
-            return n;
+            return 1;
         }else {
-            return Fibonacci(n-1) + floor(n-2);
+            return Fibonacci(n-1) + Fibonacci(n-2);
         }
        //迭代法
         /*if(n == 1 || n == 2) {
-            return n;
+            return 1;
         }
         int f1 = 1;
         int f2 = 2;
@@ -20,4 +20,18 @@ public class Solution {
         }
         return f3;*/
     }
+}
+
+class Solution {
+   public int fib(int n) {
+    int constant = 1000000007;
+    int f1 = 0;
+    int f2 = 1;
+    for(int i=0;i<n;i++) {
+        int f3 = f1 + f2;
+        f1 = f2 % constant;
+        f2 = f3 % constant;
+    }
+    return f1;
+}
 }
